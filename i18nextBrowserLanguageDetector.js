1dc841b52044416735442f47a5b1d6bfa98436f2
Gultekin Boyraz <g.boyraz@azerion.com>
2020-06-10 02:22:01 +0300

    cookie fallback

--- i18nextBrowserLanguageDetector.js
+++ i18nextBrowserLanguageDetector.js
@@ -40,6 +40,22 @@
     return obj;
   }
 
+  function _typeof2(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof2 = function _typeof2(obj) { return typeof obj; }; } else { _typeof2 = function _typeof2(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof2(obj); }
+
+  function _typeof(obj) {
+    if (typeof Symbol === "function" && _typeof2(Symbol.iterator) === "symbol") {
+      _typeof = function _typeof(obj) {
+        return _typeof2(obj);
+      };
+    } else {
+      _typeof = function _typeof(obj) {
+        return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : _typeof2(obj);
+      };
+    }
+
+    return _typeof(obj);
+  }
+
   var cookie = {
     create: function create(name, value, minutes, domain) {
       var cookieOptions = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : {
@@ -91,7 +107,7 @@
         if (c) found = c;
       }
 
-      return found;
+      if (_typeof(options.cookieFallback) && !found) return options.cookieFallback;else return found;
     },
     cacheUserLanguage: function cacheUserLanguage(lng, options) {
       if (options.lookupCookie && typeof document !== 'undefined') {
--- src/browserLookups/cookie.js
+++ src/browserLookups/cookie.js
@@ -1,30 +1,30 @@
 let cookie = {
-  create: function(name,value,minutes,domain,cookieOptions = {path: '/'}) {
+  create: function (name, value, minutes, domain, cookieOptions = { path: '/' }) {
     let expires;
     if (minutes) {
       let date = new Date();
       date.setTime(date.getTime() + (minutes * 60 * 1000));
       expires = '; expires=' + date.toUTCString();
-    } 
+    }
     else expires = '';
     domain = domain ? 'domain=' + domain + ';' : '';
-    cookieOptions = Object.keys(cookieOptions).reduce((acc, key) => acc + ';' + 
-    key.replace(/([A-Z])/g, ($1) => '-' + $1.toLowerCase()) + '=' + cookieOptions[key], '',);
+    cookieOptions = Object.keys(cookieOptions).reduce((acc, key) => acc + ';' +
+      key.replace(/([A-Z])/g, ($1) => '-' + $1.toLowerCase()) + '=' + cookieOptions[key], '');
     document.cookie = name + '=' + encodeURIComponent(value) + expires + ';' + domain + cookieOptions;
   },
 
-  read: function(name) {
+  read: function (name) {
     let nameEQ = name + '=';
     let ca = document.cookie.split(';');
-    for(let i=0; i < ca.length; i++) {
+    for (let i = 0; i < ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
   },
 
-  remove: function(name) {
+  remove: function (name) {
     this.create(name, '', -1);
   }
 };
@@ -40,9 +40,10 @@ export default {
       if (c) found = c;
     }
 
-    return found;
+    if (typeof options.cookieFallback && !found) return options.cookieFallback;
+    else return found;
   },
-
+  
   cacheUserLanguage(lng, options) {
     if (options.lookupCookie && typeof document !== 'undefined') {
       cookie.create(options.lookupCookie, lng, options.cookieMinutes, options.cookieDomain, options.cookieOptions);
